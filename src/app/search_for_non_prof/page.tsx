7a3a525cb23eaa6249a2fc3488c2b64cc9b7e3ce
nurbek <nurbek.baktygali@gmail.com>
2024-07-24 18:45:13 +0500

    today is 2024_3

--- src/app/search_for_non_prof/page.tsx
+++ src/app/search_for_non_prof/page.tsx
@@ -4,9 +4,17 @@ import React, { useState } from "react";
 import Chat from "../components/chat";
 import Sidebar from "../components/Sidebar"; // Import the Sidebar component
 
+interface ChatMessage {
+    text: string;
+    sender: 'user' | 'bot';
+    time: string;
+    page_content?: string;
+    source?: string;
+}
+
 interface ChatItem {
     title: string;
-    messages: Array<{ text: string, sender: string, time: string, page_content?: string, source?: string }>;
+    messages: ChatMessage[];
     time: string;
 }
 
@@ -22,14 +30,14 @@ export default function Component() {
     };
 
     return (
-        <div className=" flex flex-col min-h-screen bg-gray-100 pt-12 md:flex-row"> 
-            <Sidebar onSelectChat={handleSelectChat} onAddNewQuery={handleAddNewQuery} /> 
-            <div className="flex flex-col items-center justify-center w-full ml-0 md:ml-64 mt-8 md:mt-0"> 
-                <div className="max-w-6xl w-full px-4 sm:px-6 lg:px-8 flex flex-col flex-grow">
-                    <div className="mt-8 flex flex-col items-center flex-grow space-y-0"> 
+        <div className="flex flex-col min-h-screen bg-gray-100 pt-12 md:flex-row"> {/* Reduced top padding */}
+            <Sidebar onSelectChat={handleSelectChat} onAddNewQuery={handleAddNewQuery} /> {/* Add the Sidebar component with the onSelectChat and onAddNewQuery handlers */}
+            <div className="flex flex-col items-center justify-center flex-grow ml-0 md:ml-64 mt-4 md:mt-0"> {/* Adjusted margin-left for main content and reduced top margin */}
+                <div className="max-w-full w-full px-4 sm:px-6 lg:px-8 flex flex-col flex-grow"> {/* Stretch to full width */}
+                    <div className="flex flex-col items-center flex-grow space-y-0"> {/* Removed space-y-0 class */}
                         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent mb-2">Ask</h1> {/* Reduced bottom margin */}
-                        <div className="relative w-full flex flex-col flex-grow mt-2"> 
-                            <Chat selectedChat={selectedChat} onNewQuery={handleAddNewQuery} />
+                        <div className="relative w-full flex flex-col flex-grow max-h-96"> {/* Stretch to full width and reduced height */}
+                            <Chat selectedChat={selectedChat} onNewQuery={handleAddNewQuery} /> {/* Pass the selectedChat and onNewQuery to the Chat component */}
                         </div>
                     </div>
                 </div>
